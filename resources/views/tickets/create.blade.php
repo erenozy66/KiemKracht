aca8befb8f74784229c8dd8f3210828e0fc501df
Ozy <112827689+erenozy66@users.noreply.github.com>
2026-03-30 21:33:26 +0200

    Update create.blade.php

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Submit Ticket')
 
 @section('content')
-//Formulier voor ticketinvoer
+{{-- Formulier voor tickets --}}
 <div class="row justify-content-center">
     <div class="col-md-6">
         <div class="card border-0 bg-transparent">
